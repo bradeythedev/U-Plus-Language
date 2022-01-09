3c00cbf17fb225aeea7a129d14b3b197623a4958
bradeythedev <maybp77@yahoo.com>
2022-01-09 20:10:57 +0800

    Create Pelican csproj

---
+++ Pelican/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pelican
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
